38f8c5b071dc4b5555cdaf91749a94e3f4773c89
Evan You <yyx990803@gmail.com>
2015-09-30 13:32:28 -0400

    adjust ci script for more parallel builds

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,15 +9,31 @@ case $CIRCLE_NODE_INDEX in
   ./packages/test-in-console/run.sh
   ;;
 1)
-  echo "Running self-test (1): A-C"
-  ./meteor self-test --file "^[a-c]" --exclude "$SELF_TEST_EXCLUDE"
+  echo "Running self-test (1): A-Com"
+  ./meteor self-test --file "^[a-b]|^c[a-n]|^co[a-m]" --exclude "$SELF_TEST_EXCLUDE"
   ;;
 2)
-  echo "Running self-test (2): D-P"
-  ./meteor self-test --file "^[d-p]" --exclude "$SELF_TEST_EXCLUDE"
+  echo "Running self-test (2): Con-Li"
+  ./meteor self-test --file "^co[n-z]|^c[p-z]|^[d-k]|^l[a-i]" --exclude "$SELF_TEST_EXCLUDE"
   ;;
 3)
-  echo "Running self-test (3): R-Z"
-  ./meteor self-test --file "^[r-z]" --exclude "$SELF_TEST_EXCLUDE"
+  echo "Running self-test (3): Lj-O"
+  ./meteor self-test --file "^l[j-z]|^[m-o]" --exclude "$SELF_TEST_EXCLUDE"
+  ;;
+4)
+  echo "Running self-test (4): P"
+  ./meteor self-test --file "^p" --exclude "$SELF_TEST_EXCLUDE"
+  ;;
+5)
+  echo "Running self-test (5): Run"
+  ./meteor self-test --file "^run" --exclude "$SELF_TEST_EXCLUDE"
+  ;;
+6)
+  echo "Running self-test (6): R-So"
+  ./meteor self-test --file "^r(?!un)|^s[a-o]" --exclude "$SELF_TEST_EXCLUDE"
+  ;;
+7)
+  echo "Running self-test (7): Sp-Z"
+  ./meteor self-test --file "^s[p-z]|^[t-z]" --exclude "$SELF_TEST_EXCLUDE"
   ;;
 esac
